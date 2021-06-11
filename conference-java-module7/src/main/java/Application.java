27e96a4f0d5828c2b7ba17ceccd366c08dca40cc
ye Li <yeli.88@gmail.com>
2021-06-11 15:25:29 -0400

    use stereotype to define a bean

---
+++ conference-java-module7/src/main/java/Application.java
@@ -0,0 +1,18 @@
+import com.pluralsight.service.SpeakerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args){
+
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service);
+        System.out.println(service.findAll().get(0).getFirstName());
+
+        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
+    }
+}
---
+++ conference-java-module7/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository("speakerRepository")
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Override
+    public List<Speaker> findAll(){
+        List<Speaker> speakers = new ArrayList<>();
+        Speaker speaker = new Speaker();
+
+        speaker.setFirstName("Bryan");
+        speaker.setLastName("Hansen");
+
+        speakers.add(speaker);
+
+        return speakers;
+
+    }
+}
