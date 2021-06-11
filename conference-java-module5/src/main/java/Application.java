9e2fcf7ae41e5ba4c7e14c09f5384b18ea2b4f79
ye Li <yeli.88@gmail.com>
2021-06-11 13:27:47 -0400

    scope prototype

---
+++ conference-java-module5/src/main/java/Application.java
@@ -0,0 +1,19 @@
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+    public static void main(String args[]){
+
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service);
+
+        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
+
+    }
+}
