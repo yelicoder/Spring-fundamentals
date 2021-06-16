5959839577b13b42aa3fc06cdae862b9986d11fd
ye Li <yeli.88@gmail.com>
2021-06-16 16:11:29 -0400

    factoryBean

---
+++ conference-java-module10/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+@Repository("speakerRepository")
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
+
+    @Override
+    public List<Speaker> findAll(){
+        List<Speaker> speakers = new ArrayList<>();
+        Speaker speaker = new Speaker();
+
+        speaker.setFirstName("Bryan");
+        speaker.setLastName("Hansen");
+
+        System.out.println("cal: " + cal.getTime());
+
+        speakers.add(speaker);
+
+        return speakers;
+
+    }
+}
