2d6efaa01efc534c1ddcfe59b94984483dc77114
ye Li <yeli.88@gmail.com>
2021-06-16 11:14:24 -0400

    xml configuration constructor injection

---
+++ conference-xml-module2/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,30 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
+public class SpeakerServiceImpl implements SpeakerService {
+
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl(){
+        System.out.println("in constructor with no arg");
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        System.out.println("in constructor with arg");
+        this.repository = repository;
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        System.out.println("in setter");
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Speaker> findAll(){
+        return repository.findAll();
+    }
+}
