f4ad859faca2ade25ddfc42db53eefd729d2be04
ye Li <yeli.88@gmail.com>
2021-06-16 11:44:29 -0400

    autowire by name

---
+++ conference-xml-module5/src/main/java/Application.java
@@ -0,0 +1,15 @@
+import com.pluralsight.service.SpeakerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+    public static void main(String args[]) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
+}
